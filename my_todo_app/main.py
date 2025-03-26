b94a9b538151ae13dbd5e75753a12823108b3647
Sejin Choi <chltpwlsflatron@gmail.com>
2025-03-26 16:29:51 +0900

    add calendar

--- my_todo_app/main.py
+++ my_todo_app/main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 import json
 import os
+from typing import Optional
 
 app = FastAPI()
 
@@ -12,28 +13,25 @@ class TodoItem(BaseModel):
     title: str
     description: str
     completed: bool
+    date: Optional[str] = None
 
 # JSON 파일 경로
 TODO_FILE = "todo.json"
 
-# JSON 파일에서 To-Do 항목 로드
 def load_todos():
     if os.path.exists(TODO_FILE):
         with open(TODO_FILE, "r") as file:
             return json.load(file)
     return []
 
-# JSON 파일에 To-Do 항목 저장
 def save_todos(todos):
     with open(TODO_FILE, "w") as file:
         json.dump(todos, file, indent=4)
 
-# To-Do 목록 조회
 @app.get("/todos", response_model=list[TodoItem])
 def get_todos():
     return load_todos()
 
-# 신규 To-Do 항목 추가
 @app.post("/todos", response_model=TodoItem)
 def create_todo(todo: TodoItem):
     todos = load_todos()
@@ -41,7 +39,6 @@ def create_todo(todo: TodoItem):
     save_todos(todos)
     return todo
 
-# To-Do 항목 수정
 @app.put("/todos/{todo_id}", response_model=TodoItem)
 def update_todo(todo_id: int, updated_todo: TodoItem):
     todos = load_todos()
@@ -52,7 +49,6 @@ def update_todo(todo_id: int, updated_todo: TodoItem):
             return updated_todo
     raise HTTPException(status_code=404, detail="To-Do item not found")
 
-# To-Do 항목 삭제
 @app.delete("/todos/{todo_id}", response_model=dict)
 def delete_todo(todo_id: int):
     todos = load_todos()
@@ -60,9 +56,8 @@ def delete_todo(todo_id: int):
     save_todos(todos)
     return {"message": "To-Do item deleted"}
 
-# HTML 파일 서빙
 @app.get("/", response_class=HTMLResponse)
 def read_root():
-    with open("templates/index.html", "r") as file:
+    with open("templates/index.html", "r",encoding="utf-8") as file:
         content = file.read()
     return HTMLResponse(content=content)
